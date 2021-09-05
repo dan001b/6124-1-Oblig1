e253bfd7944a5e9d12c028d6edad002903e6dc06
Mustafa Alqaisy <mustafa.saleh@hotmail.no>
2021-09-05 13:09:47 +0200

    La til kommentarer. La til en del tilfeldighet som er beskrevet i oppgaveteksten, vinkel og lengde er nå også tilfeldige basert på vekting. Endret på tykkelses parametert, det blir tynnere gradvis. Endret på oppstartsparametere, vindu størrelse pg navngivning i GUI.

--- src/client/OptionPanel.java
+++ src/client/OptionPanel.java
@@ -12,14 +12,19 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 /**
+ * 6124-1 21H Algoritmer og datastrukturer Oblig1-grupper 1
  *
- * @author mussal
+ * @author Mustafa Waleed Alqaisy (studentnummer: 216557)
+ * @author Sindre Andreas Olsen Strømnæss
+ *
+ * Klassen lager slidere for flere verdier tilbyr enkle get metoder for å hente
+ * verdiene
  */
 public class OptionPanel extends HBox {
 
     private Slider angleSlider;
     private Slider depthSlider;
-    private Slider probSlider;
+    private Slider weightSlider;
     private Slider trunkSlider;
 
     public OptionPanel() {
@@ -27,46 +32,50 @@ public class OptionPanel extends HBox {
         GridPane sliderP = new GridPane();
         sliderP.setPadding(new Insets(0, 5, 0, 5));
 
-        angleSlider = new Slider(0, 90, 35);
-        formatSlider(angleSlider,10);
+        angleSlider = new Slider(0, 90, 20);
+        formatSlider(angleSlider, 10);
 
-        depthSlider = new Slider(0, 15, 9);
-        formatSlider(depthSlider,2);
-        
-        probSlider = new Slider(0, 0.1, 0.01);
-        formatSlider(probSlider, 0.01);
-        
-        trunkSlider = new Slider(1, 10, 5);
+        depthSlider = new Slider(0, 15, 12);
+        formatSlider(depthSlider, 2);
+
+        weightSlider = new Slider(0, 10, 1);
+        formatSlider(weightSlider, 2);
+
+        trunkSlider = new Slider(1, 30, 20);
         formatSlider(trunkSlider, 10);
-        
+
         // legger til slider i sliderP, dette er gridpane
-        // settes ved å oppgi posisjon
+        // posisjon settes ved å oppgi koordinater
         sliderP.add(new Label("Vinkel: "), 0, 0);
         sliderP.add(angleSlider, 1, 0);
-        
 
         sliderP.add(new Label("Antall nivåer: "), 0, 1);
         sliderP.add(depthSlider, 1, 1);
-        
-        sliderP.add(new Label("Loss: "), 3, 0);
-        sliderP.add(probSlider, 4, 0);
-        
-        sliderP.add(new Label("Trunk size:"), 3, 1);
+
+        sliderP.add(new Label("Tilfeldighet: "), 3, 0);
+        sliderP.add(weightSlider, 4, 0);
+
+        sliderP.add(new Label("Stamme tykkelse:"), 3, 1);
         sliderP.add(trunkSlider, 4, 1);
 
         getChildren().addAll(sliderP);
 
-        //setStyle("-fx-background-color: lightgrey; -fx-border-style: dashed");
-
         setPadding(new Insets(10, 10, 10, 10));
     }
-    
-    private void formatSlider(Slider s, double majorTick){
+
+    /**
+     * metoden formaterer slider
+     *
+     * @param s slider som skal formateres
+     * @param majourTick intervall for den største markør hoppet det minste
+     * hoppet settes som halvparten av den største
+     */
+    private void formatSlider(Slider s, double majorTick) {
         s.setSnapToTicks(true);
         s.setShowTickMarks(true);
         s.setShowTickLabels(true);
         s.setMajorTickUnit(majorTick);
-        s.setMinorTickCount((int) majorTick/2);
+        s.setMinorTickCount((int) majorTick / 2);
         s.setPadding(new Insets(2, 20, 2, 0));
     }
 
@@ -77,13 +86,13 @@ public class OptionPanel extends HBox {
     public int getDepth() {
         return (int) depthSlider.getValue();
     }
-    
+
     public double getWeight() {
-        return probSlider.getValue();
+        return weightSlider.getValue()/100;
     }
-    
+
     public double getTrunksize() {
-    	return trunkSlider.getValue();
+        return trunkSlider.getValue();
     }
 
 }
--- src/client/RecursiveTree.java
+++ src/client/RecursiveTree.java
@@ -1,61 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package client;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javafx.application.Application;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
 
+/**
+ * 6124-1 21H Algoritmer og datastrukturer Oblig1-grupper 1
+ *
+ * @author Mustafa Waleed Alqaisy (studentnummer: 216557)
+ * @author Sindre Andreas Olsen Strømnæss
+ */
+public class RecursiveTree extends Application {
 
-public class RecursiveTree extends JFrame implements ActionListener{
-	
-	JPanel sidemeny = new JPanel();
-	
-	int dybde = 8;
-	
-	public RecursiveTree() {
-		super("Modifiserbart rekursivt tre");
-        setResizable(false);
-		setBounds(100, 100, 800, 600);
-		//setBackground(Color.BLACK);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setLayout(new BorderLayout());
-	}
-	
-	public void tegnTre(Graphics g, int xForste, int yForste, double vinkel, int dybde) {
-		if(dybde < 0) return;
-		
-		int xAndre = xForste + (int) (Math.cos(Math.toRadians(vinkel)) * dybde * 10.0);
-		int yAndre = yForste + (int) (Math.sin(Math.toRadians(vinkel)) * dybde * 10.0);
-		
-		g.drawLine(xForste, yForste, xAndre, yAndre);
-		tegnTre(g, xAndre, yAndre, vinkel - 35, dybde - 1);
-		tegnTre(g, xAndre, yAndre, vinkel + 35, dybde - 1);
-	}
-	
-	@Override
-	public void paint(Graphics g) {
-		g.setColor(Color.BLACK); //fargen på de grafiske elementene (linjene)
-        tegnTre(g, 400, 500, -90, dybde);
-	}
+    static final int WIDTH = 1000;
+    static final int HEIGHT = 1000;
 
+    private OptionPanel optionPane = new OptionPanel();
 
-	public static void main(String[] args) {
-		RecursiveTree testTre = new RecursiveTree();
-		testTre.setVisible(true);
-	}
+    @Override
+    public void start(Stage primaryStage) {
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        // oppretter knapp for å tegne treet
+        Button redraw = new Button("Tegn tre");
+
+        // oppretter paneler for tegnebretter og for instillinger
+        Pane canvas = new Pane();
+        BorderPane mainboard = new BorderPane(canvas);
+        canvas.setStyle(
+                "-fx-background-color: #F0F8FF ; -fx-border-color: black;"
+        );
+
+        StackPane actionBox = new StackPane(redraw);
+        actionBox.setPadding(new Insets(10, 10, 10, 10));
+        mainboard.setTop(new HBox(optionPane, actionBox));
+
+        mainboard.setCenter(canvas);
+
+        Scene scene = new Scene(mainboard, WIDTH, HEIGHT);
+
+        primaryStage.setTitle("Recursive tree");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        // tegner treet ved oppstart
+        drawTree(canvas, optionPane.getAngle(), optionPane.getDepth(), optionPane.getWeight(), optionPane.getTrunksize());
+
+        // binder handling for tegne knappen
+        redraw.setOnAction(e -> {
+
+            // sletter alt innhold i tegnebrettet
+            canvas.getChildren().removeAll(canvas.getChildren());
+
+            //System.out.println(optionPane.getAngle()+"  :   "+optionPane.getDepth()+"  :   "+optionPane.getWeight()+"  :   "+optionPane.getTrunksize());
+            // tegner nytt tre
+            drawTree(canvas, optionPane.getAngle(), optionPane.getDepth(), optionPane.getWeight(), optionPane.getTrunksize());
+        });
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * rekursiv hjelpemetode
+     * tegner rekursiv tree
+     * 
+     * @param canvas panelet der linejene skal legges til
+     * @param xStart x koordinat for punktet der treet starter
+     * @param yStart y koordinat for punktet der treet starter
+     * @param angle vinkel for første linje brukes i rekrusjon for å beregne
+     * påfølgene vinkler
+     * @param angleOffset endring i vinkel for påfølgende grener
+     * @param depth antall nivåer som skal tegnes
+     * @param weight vekting av hvor sannsynlig
+     */
+    public void drawTree(Pane canvas, int xStart, int yStart, double angle, double angleOffset, int depth, double weight, double trunkSize) {
+
+        // Setter opp en par varslinger ved ugyldig verdi for variabler
+        if (weight < 0 || weight > 1) {
+            throw new IllegalArgumentException("weight must be greater or equal to 0 and smaller or equal to 1.0");
+        }
+
+        if (trunkSize < 0) {
+            throw new IllegalArgumentException("trunkSize must be greater or equal to 0");
+        }
+        
+        // Sørger for tilnærmet tilfeldig lengde basert på vekting
+        int lengthRand = 5;
+        if (Math.random() < weight) {
+            lengthRand = (int) (10 * Math.random());
+        }
+        
+        // finner koordinatene for punkt der vektoren ender, 
+        // beregnes basert på vinkelen oppgitt for metoden 
+        int xEnd = xStart + (int) (Math.cos(Math.toRadians(angle)) * depth * lengthRand);
+        int yEnd = yStart + (int) (Math.sin(Math.toRadians(angle)) * depth * lengthRand);
+
+        //lager 2D linjeobjekt med start og slutt koordinater
+        Line ln = new Line(xStart, yStart, xEnd, yEnd);
+
+        // beregner differanse i x og i y
+        int difX = xEnd - xStart;
+        int difY = yEnd - yStart;
+
+        // beregnere lengden for linjen
+        double length = Math.sqrt(difX * difX + difY * difY);
+
+        // Base case hvis lengden er mindre enn 1 returneres
+        if (length < 1) {
+            return;
+        }
+
+        //storrelse paa trestamme (tykkelse)
+        ln.setStrokeWidth(trunkSize);
+
+        // Halverer trestamme kun hvis tykkelse er mer enn 1 pixel
+        if (trunkSize > 1) {
+            trunkSize = trunkSize * 0.5;
+        }
+
+        // legger til linjen på tegnebrettet
+        canvas.getChildren().add(ln);
+
+        // Følgende betingelser sørger for å hoppe over grener tilnærmet tilfeldig
+        // Sannsynligheten vektes av sluttbruker
+        double randAngle = 1;
+
+        if (Math.random() > weight) {
+            
+            // Setter tilfeldig vinkel basert på vekting
+            if (Math.random() < weight) {
+                 randAngle = angleOffset * Math.random();
+            }
+            
+            //tegner ny gren
+            drawTree(canvas, xEnd, yEnd, angle - angleOffset*randAngle, angleOffset,  depth - 1, weight, trunkSize);
+        }
+        if (Math.random() > weight) {
+            
+            // Viss sansynlighet for å setter ny tilfeldig vinkel basert på vekting
+            randAngle = 1;
+            if (Math.random() < weight) {
+                 randAngle = angleOffset * Math.random();
+            }
+            
+            //tegner ny gren
+            drawTree(canvas, xEnd, yEnd, angle + angleOffset*randAngle, angleOffset, depth - 1, weight, trunkSize);
+        }
+    }
+
+    /**
+     * Drivermetode Tar imot kun de essensielle variablene, regner og setter
+     * resten
+     */
+    public void drawTree(Pane canvas, double angleOffset, int dybde, double weight, double trunkSize) {
+
+        // treets start posisjon beregnes basert på størrelse for tegneflate
+        int xForste = (int) canvas.getWidth() / 2;
+        int yForste = (int) (canvas.getHeight() * 0.75);
+
+        // kaller hjelpemetoden
+        drawTree(canvas, xForste, yForste, -90, angleOffset, dybde, weight, trunkSize);
+
+    }
 
 }
